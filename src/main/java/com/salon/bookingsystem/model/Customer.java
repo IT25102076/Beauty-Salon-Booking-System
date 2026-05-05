4914f33ef50f7bc34af2ca0d9503e5f3d6d1164b
it25101519 <it25101519@my.sliit.lk>
2026-05-05 16:10:01 +0530

    add model

---
+++ src/main/java/com/salon/bookingsystem/model/Customer.java
@@ -0,0 +1,24 @@
+package com.salon.bookingsystem.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "customer")
+
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+
+    private String phonenumber;
+    private String address;
+    private String preferredStylist;
+}
