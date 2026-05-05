1c9b942f70f70f8f5a857a3c154392fd6733a869
IT25102076 <it25102076@my.sliit.lk>
2026-05-05 11:04:42 +0530

    Initial Project Structure: Spring Boot, MySQL, and JSP

---
+++ src/test/java/com/salon/bookingsystem/BookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salon.bookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
