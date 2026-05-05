6a69b9e3abf327b2de7410d7bcc930fd447f9eef
it25101519 <it25101519@my.sliit.lk>
2026-05-05 16:10:46 +0530

    add repository

---
+++ src/main/java/com/salon/bookingsystem/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.salon.bookingsystem.repository;
+
+import com.salon.bookingsystem.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Customer findByEmail(String email);
+}
